13fab197faa4dafd50bc79d576bda9d7cfb46680
warm-2233 <57078602+warm-2233@users.noreply.github.com>
2020-08-01 12:45:43 +0800

    Add files via upload

---
+++ python-myqr-二维码生成.py
@@ -0,0 +1,13 @@
+﻿import os
+
+# 第三方模块
+from MyQR import myqr
+
+p = os.getcwd()
+
+myqr.run(words="",
+        version=20,
+        level="H",
+        picture=p+"/0.jpg",
+        colorized=True,
+        save_dir=p)
